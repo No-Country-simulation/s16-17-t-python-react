1032910e7ade34ea1f4ec553152fc15a46a4276c
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-07-16 20:13:22 -0300

    confilictos resueltos y rutas arregladas en file barrel

---
+++ frontend/src/store/store.js
@@ -0,0 +1,10 @@
+import { create } from 'zustand'
+
+const useUserStore = create((set) => ({
+    user: null,
+    setUser: (user) =>
+        set({ user }) /* Estado para vincular con confirmación de usuario */,
+    clearUser: () => set({ user: null }),
+}))
+
+export default useUserStore
