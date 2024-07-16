bd4b9b5c13465dc83d637644a8e7dab304c01ffd
Joel Lawi <abecedegluu@gmail.com>
2024-07-16 17:09:01 -0500

    Fix:Rutas generales apps

--- backend/SnapTrip/settings.py
+++ backend/SnapTrip/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'apps.camera',
-    'apps.lens',
+    
     
 ]
 
--- backend/SnapTrip/urls.py
+++ backend/SnapTrip/urls.py
@@ -19,6 +19,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cameras/', include('apps.camera.urls')),
-    path('lens/', include('apps.lens.urls')),
 ]
