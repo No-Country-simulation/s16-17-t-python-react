2944c1e0b94d2c177069f2dae39dc7221089d337
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-07-16 18:20:44 -0400

    feat: router config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import { Suspense } from 'react'
 import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { AppRouter } from './router'
 
 function App() {
-	return <>Hola</>
+	return (
+		<>
+			<Router>
+				<Suspense>
+					<AppRouter />
+				</Suspense>
+			</Router>
+		</>
+	)
 }
 
 export default App
