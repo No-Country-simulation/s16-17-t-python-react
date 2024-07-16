8c4d34853d23d003fb5a13bbbc3cf4db79195528
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-07-16 14:57:00 -0300

    subiendo limpieza de codigo register y auth

---
+++ frontend/src/auth/AuthUser.jsx
@@ -0,0 +1,10 @@
+
+export const AuthUser = () => {
+  return (
+    <div className="h-[40vh]">
+
+      <h2 className="flex justify-center ">Vista autenticacion de usuario</h2>
+    </div>
+  )
+}
+
