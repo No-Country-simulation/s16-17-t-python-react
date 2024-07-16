040af439b1b309c07a059a8fec2a639fd5120e60
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-07-16 18:40:54 -0400

    feat: login redirect to register

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,5 +1,6 @@
 import { Divider } from '@mui/material'
 import { Header, Form } from '../../components'
+import { Link } from "react-router-dom";
 
 export const Login = () => (
 	<div className="min-h-screen">
@@ -11,7 +12,7 @@ export const Login = () => (
 				<div className="px-8 flex justify-center">
 					<p className="text-[#807D84] text-xs flex gap-1 mt-2">
 						¿No tenés una cuenta?
-						<span className="text-[#6E9E30]">Registrarme</span>
+						<Link to='/register' className="text-[#6E9E30]">Registrarme</Link>
 					</p>
 				</div>
 
