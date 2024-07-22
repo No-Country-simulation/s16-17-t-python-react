589f04050aecc5494a04974bc725a9f94cd53c62
Sebastian Acosta <sebas.sas@gmail.com>
2024-07-22 16:34:59 -0500

    fix: SnapTrip/urls.py

--- backend/SnapTrip/urls.py
+++ backend/SnapTrip/urls.py
@@ -15,10 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('apps.account.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('events/', include('apps.events.urls')),
-    path('album/', include('apps.album.urls'))
-]
+    path('album/', include('apps.album.urls')),
+    # path('cameras/', include('apps.camera.urls')),
+    # path('lens/', include('apps.lens.urls')), 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
