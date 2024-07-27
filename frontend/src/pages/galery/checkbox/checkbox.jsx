64fca0351f451862415141a5e14af2411589773f
Jean <jeanpolo15@gmail.com>
2024-07-27 17:41:33 -0500

    fix_1 lautaro checkbox

--- frontend/src/pages/galery/checkbox/checkbox.jsx
+++ frontend/src/pages/galery/checkbox/checkbox.jsx
@@ -1,33 +1,29 @@
 import React, { useState } from 'react'
-import Checkbox from '@mui/material/Checkbox';
+import Checkbox from '@mui/material/Checkbox'
 import style from '../style.module.css'
 const { checkbox_styles } = style
 export const CheckboxComponent = ({ isChecked, label, handCheck, index }) => {
-    const CheckHandler = () => {
-        setchecked_state(!checked_state)
-    }
-    return (
-        <>
-            <div
-                className="flex items-center gap-2">
-                <Checkbox
-                    value={isChecked}
-                    checked={CheckHandler}
-                    id={`checkbox${index}`}
-                    onChange={handCheck}
-                    sx={{
-                        ":checked": {
-                            caretShapeolor: "#6E9E30 !important",
-                            padding: "5px"
-                        }
-                    }}
-                    className={checkbox_styles} />
-                <label
-                    htmlFor="checkbox"
-                >{label}
-                
-                </label>
-            </div>
-        </>
-    )
+	const CheckHandler = () => {
+		setchecked_state(!checked_state)
+	}
+	return (
+		<>
+			<div className="flex items-center gap-2">
+				<Checkbox
+					value={isChecked}
+					checked={CheckHandler}
+					id={`checkbox${index}`}
+					onChange={handCheck}
+					sx={{
+						':checked': {
+							caretShapeolor: '#6E9E30 !important',
+							padding: '5px',
+						},
+					}}
+					className={checkbox_styles}
+				/>
+				<label htmlFor="checkbox">{label}</label>
+			</div>
+		</>
+	)
 }
