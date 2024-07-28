e99264d4e7cc8529256aef956f8a2562e79a9c10
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-07-27 20:11:50 -0400

    refactor: prettier change

--- frontend/src/pages/galery/checkbox/CheckboxGallery.jsx
+++ frontend/src/pages/galery/checkbox/CheckboxGallery.jsx
@@ -4,11 +4,11 @@ import style from '../style.module.css'
 const { checkbox_styles } = style
 
 export const CheckboxGallery = ({ handleOptions, label, optionName }) => (
-		<Checkbox
-			id={`checkbox-${label}`}
-			name={optionName}
-			className={checkbox_styles}
-			onChange={handleOptions}
-			label={label}
-		/>
-	)
+	<Checkbox
+		id={`checkbox-${label}`}
+		name={optionName}
+		className={checkbox_styles}
+		onChange={handleOptions}
+		label={label}
+	/>
+)
